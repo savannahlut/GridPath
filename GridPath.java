946f252d8e7cc60f62dbbecfe92390aa2a973bb9
savannahlut <savannahlut@nycstudents.net>
2024-10-16 19:13:12 +0000

    Fix main and getNextLocation

--- GridPath.java
+++ GridPath.java
@@ -1,24 +1,26 @@
 public class GridPath
 {
-private int[][] grid;
-public Location getNextLoc(int row, int col)
-{ 
-    Location rightward = new Location(row, col + 1); 
-    Location under = new Location(row + 1, col);
-    if (grid[0].length -1 == col)
-    {
-        return under; 
+    private int[][] grid;
+    public Location getNextLoc(int row, int col)
+    { 
+        Location rightward = new Location(row, col + 1); 
+        Location under = new Location(row + 1, col);
+        if (grid[0].length - 1 == col)
+        {
+            return under; 
+        }
+        if (grid.length - 1 == row)
+        {
+            return rightward; 
+        }
+        if (grid[row + 1][col] < grid[row][col + 1])
+        {
+            return under; 
+        } else {
+            return rightward; 
+        }
     }
-    if (grid.length - 1 == row)
-    {
-        return rightward; 
-    }
-    if ()
-    {
-        
-    }
-}
 
-public int sumPath(int row, int col)
-{  }
+    public int sumPath(int row, int col)
+    {}
 }
